dd2cdfdd41ef86f122680c3bc8136cbe11789a8f
boytrend <boytrend@gmail.com>
2022-11-19 20:05:28 +0400

    made nav-bar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,35 @@
+<template>
+   <nav class="navbar">
+    <div class="wrapper">
+        <div class="nav-block">left</div>
+       <div class="nav-block">right</div>
+    </div>
+       
+   </nav>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+.navbar{
+    background: #222222;
+    height: 70px;
+    width: 100%;
+}
+.wrapper{
+   height: 100%;
+   max-width: 1140px;
+   display: flex;
+   align-items: center;
+   justify-content: space-between;
+   margin: 0 auto;
+   border: 1px solid rgb(185, 17, 17);;
+}
+.nav-block{
+background-color: aliceblue;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+    <H1>This is a home page</H1>
+ 
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+
+export default {
+
+  }
+
+</script>
