669ac0b459e808aa916796b66bb34f5377199154
boytrend <boytrend@gmail.com>
2022-12-02 21:31:02 +0400

    add addToCart

--- src/components/CartItem.vue
+++ src/components/CartItem.vue
@@ -2,8 +2,13 @@
     <div class="cart-item__box slide-in-blurred-right"  >
         <img :src="require('@/assets/img/catalog/' + item.itemImg)" alt="img" class="cart-img">
         <div class="cart-item__descr">
-            <h2 class="title">{{ item.itemTitle }}</h2>
+            <h2 class="title">{{ item.itemTitle }} </h2>
             <p class="text">Price {{item.itemPrice}} $</p>
+            <div class="quantity">
+                <p class="text">Quantity: {{item.quantity}} </p>
+                <button class="quantity-bnt" @click="addToCart" >&plus;</button>
+                <button class="quantity-bnt" >&times;</button>
+            </div>
             <p class="text">Quantity: {{item.quantity}} </p>
             <p class="price">Total: {{culcTotalPrice}} $ </p>
         </div>
@@ -37,8 +42,11 @@ export default {
     },
     methods:{
         ...mapActions([
-            'GET_TOTAL_CART_PRICE'
+            'GET_TOTAL_CART_PRICE', 'ADD_TO_CART'
         ]),
+        addToCart(){
+            this.ADD_TO_CART(this.item)
+        }
     },
     
     mounted(){  
@@ -50,7 +58,7 @@ export default {
 <style lang="scss" scoped>
 .cart-item__box{
     width: 100%;
-    height: 100px;
+    height: 120px;
     // border: 1px solid $pink-color;
     @include box-shadow-pink;
     padding: 20px 0;
@@ -82,8 +90,23 @@ export default {
             line-height: 17px;
         }     
 }
+.quantity {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    .quantity-bnt {
+        width: 30px;
+        height: 20px;
+        font-size: 17px;
+        font-weight: bold;
+        cursor: pointer;
+        border: 1px solid $pink-color;
+        background-color: transparent;
+    }
+}
 
 .slide-in-blurred-right {
 	@include slide-in-blurred-right
-}      
+}
 </style>
