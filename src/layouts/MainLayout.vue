a0b23d52abd3310b574404a889d8a52ada9299be
boytrend <boytrend@gmail.com>
2022-12-23 12:14:43 +0400

    fix mounted err

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -22,9 +22,11 @@ export default {
     ...mapGetters(['GET_UID'])
   },
 
-  async mounted() { 
-    if (localStorage.getItem('firebase').length !== 0) {
-      await this.$store.dispatch('fetchInfo')
+  async mounted() {
+    if (localStorage.getItem('firebase')) {
+      if (localStorage.getItem('firebase').length !== 0) {
+        await this.$store.dispatch('fetchInfo')
+      }
     }
   }
 }
--- src/store/modules/cart.js
+++ src/store/modules/cart.js
@@ -32,21 +32,22 @@ export default {
     }
   },
   actions:{
-    GET_USER_CART({ commit}) {
-      if (localStorage.getItem('firebase').length !== 0){
-        const uid = JSON.parse(localStorage.getItem('firebase'))
-        try {
-          onValue(ref(database, `/users/${uid}/userCart`), (snapshot) => {
-            const cart = (snapshot.val());
-            commit("SET_USER_CART", cart);
-          }, {
-            onlyOnce: true
-          })
-        } catch (err) {
-          console.log(err)
+    GET_USER_CART({ commit }) {
+      if (localStorage.getItem('firebase')) {
+        if (localStorage.getItem('firebase').length !== 0 ) {
+          const uid = JSON.parse(localStorage.getItem('firebase'))
+          try {
+            onValue(ref(database, `/users/${uid}/userCart`), (snapshot) => {
+              const cart = (snapshot.val());
+              commit("SET_USER_CART", cart);
+            }, {
+              onlyOnce: true
+            })
+          } catch (err) {
+            console.log(err)
+          }
         }
       }
-     
     },
     SWITCH_SHOW_CART({commit}){
       commit('SET_SHOW_CART')
